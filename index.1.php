29906ce25bb8f4ddfb5d8c933b0f03ddde349bb8
Fede <garciafede28@gmail.com>
2019-04-25 12:54:10 -0300

    First Web

---
+++ index.1.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+<!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <title>Inicio</title>
+  </head>
+  <body>
+<nav class="navbar nav-expand-md navbar-light bg-light sticky-top">
+<div class="container-fluid">
+<button class="btn btn-success" onclick="toggleOn()">On by API</button>
+<button class="btn btn-danger" onclick="toggleOff()">Off by API</button>
+<button class="btn btn-success" onclick="toggleOnByInput()">On by Input</button>
+<button class="btn btn-danger" onclick="toggleOffByInput()">Off by Input</button>
+</div>
+</nav>
+    <iframe src="" frameborder="0"></iframe>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Inicio</title>    
+    <link rel="stylesheet" href="css/styles.css">
+  </head>
+  <body>
+<div class="container">
+<button class="botones">On</button>
+<button class="botones">Off by API</button>
+<button class="botones">On by Input</button>
+<button class="more">+</button>
+</div>
+</nav>
+    <iframe src="" frameborder="0"></iframe>
+  </body>
+</html>
